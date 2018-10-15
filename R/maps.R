fb85d9f00586e224f88a30705b3dc6eaa56df5a6
drake-smu <drakec@smu.edu>
2018-10-14 19:23:54 -0700

    breaking up report into child slides.

--- R/maps.R
+++ R/maps.R
@@ -12,16 +12,29 @@ data_brewery <- read_csv("data/raw/Breweries.csv");
 mapdata <- get_data_from_map(download_map_data("countries/us/us-all"))
 
 
-set.seed(1234)
+
 StateSummary$State <- str_trim(StateSummary$State,side="both")
 StateSummary <- StateSummary %>% mutate(`hc-a2`= State);
 
 glimpse(StateSummary);
 
-hcmap("countries/us/us-all", data = StateSummary, value = "medianABV",
-      joinBy = "hc-a2", name = "State Summary | Median ABV",
+hcmap("countries/us/us-all",
+	  data = StateSummary,
+	  value = "medianABV",
+      joinBy = "hc-a2",
+	  name = "State Summary | Median ABV",
       dataLabels = list(enabled = TRUE, format = '{point.name}'),
-      borderColor = "#FAFAFA", borderWidth = 0.1,
-      tooltip = list(valueDecimals = 5,valueSuffix = " ABV")) %>%
+      borderColor = "#FAFAFA",
+	  borderWidth = 0.1,
+      tooltip = list(valueDecimals = 5,valueSuffix = " ABV")
+	  ) %>%
+	hc_title(text = "This is a title with <i>margin</i> and <b>Strong or bold text</b>",
+			 margin = 20, align = "left",
+			 style = list(color = "#90ed7d", useHTML = TRUE)) %>%
 	hc_colorAxis(minColor = "#FFFFFF", maxColor = "#FF0000") %>%
 	hc_mapNavigation(enabled = TRUE)
+
+
+
+ggplot(BeerBrew, aes(IBU))+
+geom_density(na.rm = TRUE)
