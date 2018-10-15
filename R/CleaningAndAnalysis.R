c549bd0f8423f45d6597f7b5b6ff726e405a6fee
drake-smu <drakec@smu.edu>
2018-10-14 22:54:48 -0700

    adding in david's slide data

--- R/CleaningAndAnalysis.R
+++ R/CleaningAndAnalysis.R
@@ -5,10 +5,10 @@ rawBeer <- read.csv(file = "data/raw/Beers.csv",header=TRUE, sep=",",stringsAsFa
 ######################################## Breweries ###################################################
 rawBrew <- read.csv(file= "data/raw/Breweries.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 ############################### Examine the structures, esp colnames ############################
-# we see that beer has beer name, beer id, abv, ibu, brewery id, and style, 
+# we see that beer has beer name, beer id, abv, ibu, brewery id, and style,
 # while breweries have brew id, name, city state
 
-# we are going to change the brew id column name so it is the same, as well as make new datasets. It is 
+# we are going to change the brew id column name so it is the same, as well as make new datasets. It is
 # named Beer1 and Brew1 because they are the first iteration of the beer and brewery sets
 Beer1 <- rawBeer %>% rename(Brewery_ID=Brewery_id)
 Brew1 <- rawBrew %>% rename(Brewery_ID=Brew_ID)
@@ -29,7 +29,10 @@ na_count<- na_count%>%rename(na_count=sapply.BeerBrew..function.y..sum.length.wh
 # Here we make a data frame which has summary statistics for each state
 StateSummary <- as.data.frame(BeerBrew %>% group_by(State) %>% summarise(medianABV = median(ABV, na.rm=TRUE),medianIBU = median(IBU, na.rm=TRUE),NumBreweries = n_distinct(Brewery_ID)))
 # Now we are going to find the state that has the strongest and most bitter beer, as well as the name of that beer, because obviously we would want to know that
-MaxABV <- BeerBrew %>% select(State, BeerName, ABV) %>% filter(!is.na(ABV)) %>% arrange(desc(ABV)) %>% slice(1)
+MaxABV <- BeerBrew %>%
+	select(State, BeerName, ABV) %>%
+	filter(!is.na(ABV)) %>%
+	top_n(ABV,n=1)
 MaxIBU <- BeerBrew %>% select(State, BeerName, IBU) %>% filter(!is.na(IBU)) %>% arrange(desc(IBU)) %>% slice(1)
 # Once we decide on summary statistics we can easily make that, now all that is left is data viz
 head(BeerBrew)
